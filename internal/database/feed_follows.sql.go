9a1b54bc0ed6da31135ad5807482260cfdffd48c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-08-15 15:48:39 +0530

    Update database package using sqlc

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -53,3 +53,36 @@ func (q *Queries) DeleteFeedFollow(ctx context.Context, id uuid.UUID) error {
 	_, err := q.db.ExecContext(ctx, deleteFeedFollow, id)
 	return err
 }
+
+const getUserFeedFollows = `-- name: GetUserFeedFollows :many
+SELECT id, created_at, updated_at, user_id, feed_id FROM feed_follows WHERE id = $1
+`
+
+func (q *Queries) GetUserFeedFollows(ctx context.Context, id uuid.UUID) ([]FeedFollow, error) {
+	rows, err := q.db.QueryContext(ctx, getUserFeedFollows, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []FeedFollow
+	for rows.Next() {
+		var i FeedFollow
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.UserID,
+			&i.FeedID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
